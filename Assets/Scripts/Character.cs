95c0ac5dc6b0a15a48d69315c0767067bcab4591
leechuanhung <163072144+leechuanhung@users.noreply.github.com>
2024-05-24 16:16:33 +0900

    캐릭터 애니메이션 이동

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Character : MonoBehaviour
+{
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    public float Speed = 4f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+   
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+    }
+
+}
